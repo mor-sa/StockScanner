66b39db2594b61afff2e845e0f7b5083bdd60c2e
Mor Saranga <mor.saranga@gmail.com>
2021-02-18 15:37:51 +0200

    Initial commit

---
+++ libs/utils.py
@@ -0,0 +1,26 @@
+import yfinance
+from pydantic import BaseModel
+
+from database import SessionLocal
+from models import Stock
+
+
+class StockRequest(BaseModel):
+    symbol: str
+
+
+def fetch_stock_data(id: int):
+    db = SessionLocal()
+    stock = db.query(Stock).filter(Stock.id == id).first()
+
+    stock_data = yfinance.Ticker(stock.symbol)
+
+    stock.ma200 = stock_data.info['twoHundredDayAverage'] if stock_data.info['twoHundredDayAverage'] else 0
+    stock.ma50 = stock_data.info['fiftyDayAverage'] if stock_data.info['fiftyDayAverage'] else 0
+    stock.price = stock_data.info['previousClose'] if stock_data.info['previousClose'] else 0
+    stock.forward_pe = stock_data.info['forwardPE'] if stock_data.info['forwardPE'] else 0
+    stock.forward_eps = stock_data.info['forwardEps'] if stock_data.info['forwardEps'] else 0
+    stock.dividend_yield = stock_data.info['dividendYield'] * 100 if stock_data.info['dividendYield'] else 0
+
+    db.add(stock)
+    db.commit()
---
+++ main.py
@@ -0,0 +1,79 @@
+from typing import Optional
+
+from fastapi import FastAPI, Request, Depends, BackgroundTasks
+from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session
+
+from database import SessionLocal, engine
+import models
+from models import Stock
+import libs.utils
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+templates = Jinja2Templates(directory="templates")
+
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/")
+@app.get("/dashboard")
+def dashboard(request: Request, forward_pe=None, dividend_yield=None, ma50=None, ma200=None,
+              db: Session = Depends(get_db)):
+    """
+    Displays the stock screener dashboard.
+    """
+
+    stocks = db.query(Stock)
+
+    if forward_pe:
+        stocks = stocks.filter(Stock.forward_pe < forward_pe)
+
+    if dividend_yield:
+        stocks = stocks.filter(Stock.dividend_yield > dividend_yield)
+
+    if ma50:
+        stocks = stocks.filter(Stock.price > Stock.ma50)
+
+    if ma200:
+        stocks = stocks.filter(Stock.price > Stock.ma200)
+
+    stocks = stocks.all()
+
+    print(stocks)
+
+    return templates.TemplateResponse("dashboard.html", {
+        "request": request,
+        "stocks": stocks,
+        "dividend_yield": dividend_yield,
+        "forward_pe": forward_pe,
+        "ma50": ma50,
+        "ma200": ma200
+    })
+
+
+@app.post("/add_stock")
+# Does the same thing:
+# @app.route("/stock", methods=["POST", "GET"])
+async def create_stock(stock_request: libs.utils.StockRequest, background_tasks: BackgroundTasks,
+                       db: Session = Depends(get_db)):
+    stock = Stock()
+    stock.symbol = stock_request.symbol
+
+    db.add(stock)
+    db.commit()
+
+    background_tasks.add_task(libs.utils.fetch_stock_data, stock.id)
+
+    return {
+        "code": "success",
+        "message": "stock added to database"
+    }
